87013b65ecde9c46223a9531bac6bb8652a01f21
Mburu <john.mburu@wirecard.com>
2020-06-01 11:23:06 +0200

    fixed some errors on the analysis screen and started making some plots

--- tabiriApp/app.R
+++ tabiriApp/app.R
@@ -12,6 +12,11 @@ library(ggplot2)
 library(shinythemes)
 library(packrat)
 library(rsconnect)
+library(datasets)
+library(zoo)
+library(readr)
+library(tidyverse)
+library(stringr)
 
 
 
@@ -53,45 +58,47 @@ ui <- fluidPage(
             
             
             #ploting sales
-            selectInput("x", "Select the first X variable:",
-                        choices =  c("Sales" = "Sales",
-                                     "Purchases" = "Purchases"
-                        )),
             
-            selectInput("y", "Select the first Y variable:",
+            selectInput("ycol", "Select the first Y variable:",
                         choices =  c(
-                            "Sales Quantity" = "Sales_Quantity",
-                            "Purchase Quantity" = "Purchase_Quantity")),
+                            "Sales Quantity" = "Sales",
+                            "Sales Cancelations" = "Sale_Refunds",
+                            "Purchase Quantity" = "Purchases",
+                            "Cancelled Purchases" = "Purchase_Cncelation")),
             
             #ploting the regression model
-            actionButton(inputId = "click3", label = "Plot Regression Model Residuals"),
-            hr(),
+            #actionButton(inputId = "click3", label = "Plot Regression Model Residuals"),
+            #hr(),
             #ploting the sales prediction
-            actionButton(inputId = "click4", label = "Plot Sales Prediction"),
-            hr(),
+            #actionButton(inputId = "click4", label = "Plot Sales Prediction"),
+            #hr(),
+
+            #ploting the Purchases prediction
+            #actionButton(inputId = "click7", label = "Plot Purchases Prediction"),
+           # hr(),
             
             #predict per Location-
-            selectInput("pre", "Predicting by SME Location",
-                        choices =  c("Urban Area" = "urban",
-                                     "Rural Area" = "rural",
-                                     "Urban & Rural" = "u_r")),
-            actionButton(inputId = "click5", label = "Predict"),
+            #selectInput("pre", "Predicting by SME Location",
+            #            choices =  c("Urban Area" = "urban",
+            #                         "Rural Area" = "rural",
+             #                        "Urban & Rural" = "u_r")),
+            #actionButton(inputId = "click5", label = "Predict"),
             
             #predict per Sector
-            selectInput("pre", "Predicting by SME Sector",
-                        choices =  c( "Energy" = "energy",
-                                      "Materials" = "materials",
-                                      "Industrials" = "industrials",
-                                      "Consumer Discretionary" = "consumer_discretionary",
-                                      "Consumer Staples" = "consumer_staples",
-                                      "Health Care" = "health_care",
-                                      "Financials" = "financials",
-                                      "Information Technology" = "information_technology",
-                                      "Communication Services" = "communication_services",
-                                      "Utilities" = "utilities",
-                                      "Real Estate" = "real_estate")
-            ),
-            actionButton(inputId = "click6", label = "Predict"),     
+            #selectInput("pre", "Predicting by SME Sector",
+             #           choices =  c( "Energy" = "energy",
+             #                         "Materials" = "materials",
+              #                        "Industrials" = "industrials",
+               #                       "Consumer Discretionary" = "consumer_discretionary",
+                #                      "Consumer Staples" = "consumer_staples",
+                 #                     "Health Care" = "health_care",
+                  #                    "Financials" = "financials",
+                   #                   "Information Technology" = "information_technology",
+                    #                  "Communication Services" = "communication_services",
+                     #                 "Utilities" = "utilities",
+                      #                "Real Estate" = "real_estate")
+            #),
+            #actionButton(inputId = "click6", label = "Predict"),     
             
             
             #DISTRIBUTION TYPE
@@ -105,11 +112,11 @@ ui <- fluidPage(
             hr(),
             
             # Input: Slider for the number of observations to generate ----
-            #sliderInput("n",
-            #          "Number of observations:",
-            #          value = 7,
-            #          min = 1,
-            #          max = 30)
+            sliderInput("n",
+                      "Number of Days to predict:",
+                      value = 14,
+                      min = 2,
+                      max = 31)
             
         ),
         
@@ -121,8 +128,10 @@ ui <- fluidPage(
             # Output: Tabset data, salesanalysis, and predictions ----
             tabsetPanel(type = "tabs",
                         tabPanel("Raw Uploaded Data", dataTableOutput('contents')),
-                        tabPanel("Analysis", dataTableOutput("analysis")),
-                        tabPanel("Predictions", plotOutput("predictions"))
+                        
+                        tabPanel("Simple Analysis", plotOutput("analysis")),
+                        
+                        tabPanel("Forecasting", plotOutput("predictions"))
                         
             )
             
@@ -149,6 +158,9 @@ server <- function(input, output) {
         data <-read.csv(inFile$datapath, header = input$header)
         data
         
+        salesdata <- ts(data$Sales, frequency =12, start=c(2020,4))
+        
+        
         #outputing the head or all data only
         if (input$disp == "head") {
             return(head(data))
@@ -163,13 +175,27 @@ server <- function(input, output) {
         DT::datatable(mydata())       
     })
     
-    output$analysis<-renderText({
-        switch(input$dist,
-               "line" = "Line Graph",
-               "box" 	= 	"Box Plot",
-               "hist" =	"Histogram",
-               "density" 	=	"Density",
-               "bar" 		=	"Bar Graph")
+
+    
+    output$analysis<- renderPlot({
+      
+      #plot(salesdata)
+      
+      
+      
+      ggplot(data = salesdata, aes(x = Date, y = input$ycol)) +
+        geom_point() +
+        labs(x = "Date",
+             y = "Total Sales (Quantities)",
+             title = "SME Sales Data",
+             subtitle = "Kenya 2020")
+      
+      
+      
+      
+      
+      
+        
     })
     
     
